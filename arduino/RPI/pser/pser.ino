33e638f4309e4874d88e2b58d817ff61f16307cb
Giuseppe Luigi <joseluis.canovas@outlook.com>
2015-12-02 19:37:31 +0100

    Luz

--- arduino/RPI/pser/pser.ino
+++ arduino/RPI/pser/pser.ino
@@ -1,15 +1,30 @@
 #define LED_PIN 10
+#define luzRes A1 
+#include <Streaming.h>
 
 struct paquete {
   char longitud[4];
   char tipo;
   char payload[555];
-  void leer() {
+  bool leer() {
     Serial1.readBytes(longitud,4);
     l = hex2int(longitud,4);
     Serial1.readBytes(&tipo,1);
     Serial1.readBytes(payload,l-5);
   }
+
+  void enviar() {
+    if (tipo == 'l' || tipo == 'L')
+      this->enviar(7);
+  }
+
+  void enviar(int longitud) {
+    Serial1.write("\255",1);
+    Serial1.write(this->longitud,4);
+    Serial1.write(&tipo,1);
+    Serial1.write(payload,longitud-5);
+  }
+  
   long length() {
     return l;
   }
@@ -30,10 +45,29 @@ unsigned long hex2int(char *a, unsigned int len)
    return val;
 }
 
+void int2hex(char * a, unsigned int n) {
+  char x1 = n%16;
+  char x2 = (n>>4)%16;
+  a[1] = x1+'0';
+  a[0] = x2+'0';
+  
+  if (x1 >= 10)
+    a[1] = (x1-10)+'A';
+  if (x2 >= 10)
+    a[0] = (x2-10)+'A';
+}
+
+struct paquete luz;
+
 void setup() {
+  strcpy(luz.longitud,"000");
+  luz.longitud[3] = '7';
+  luz.tipo = 'l';
+  
   // set the data rate for the SoftwareSerial port
   Serial1.begin(4800);
-  Serial1.println("");
+  Serial.begin(9600);
+  //Serial1.println("");
   pinMode(LED_PIN, OUTPUT);
   analogWrite(LED_PIN,255);
 }
@@ -46,5 +80,11 @@ void loop() { // run over and over
      long intensidad = hex2int(p.payload,2);
      analogWrite(LED_PIN, intensidad);
     }
+  } else {
+    int res = analogRead(luzRes);
+    res = (unsigned char)(res/4);
+    int2hex(luz.payload, res);
+    luz.enviar();
+    delay(50);
   }
 }
--- arduino/RPI/rpi/restserver.py
+++ arduino/RPI/rpi/restserver.py
@@ -21,23 +21,49 @@ class paquete:
                 if (length > len(bytes)):
                         bytes += "0"*(length - len(bytes))
                 self.puerto.write(bytes)
-                #print bytes
+                print bytes
+
+	def leer(self):
+		sync = self.puerto.read(1)
+		while sync != "\255":
+			sync = self.puerto.read(1)
+		self.long = self.puerto.read(4)
+		self.tipo = self.puerto.read(1)
+		self.payload = self.puerto.read(int(self.long,16)-5)
+
         long = "0007"
         tipo = "L"
         payload = "0"
 
 
 
+luz = 0
 class PUTHandler(SimpleHTTPRequestHandler):
     p = paquete(port)
     def do_PUT(self):
+	print "ayyy"
         length = int(self.headers['Content-Length'])
         content = self.rfile.read(length)
         self.send_response(200)
 	x = int(content)%256
+	self.p.tipo = "L"
 	self.p.payload = hex(x)[2:]
 	self.p.payload = (2-len(self.p.payload))*"0"+self.p.payload
 	self.p.enviar()
+
+    def do_GET(self):
+	global luz
+	if self.path != "/luz":
+		return SimpleHTTPRequestHandler.do_GET(self)
+        while self.p.puerto.inWaiting() > 0:
+                self.p.leer()
+                if (self.p.tipo == "l"):
+                    luz = self.p.payload
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(luz)
+		
 
 HandlerClass = PUTHandler
 ServerClass  = BaseHTTPServer.HTTPServer
